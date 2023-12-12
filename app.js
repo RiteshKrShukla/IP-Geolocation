0e5d7a61b66929beddc6d1c2d9889281dfcc2df2
RiteshKrShukla <143931531+RiteshKrShukla@users.noreply.github.com>
2023-12-12 14:07:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const geoip = require('geoip-lite');
+const axios = require('axios');
+
+const getClientIp = (req) => {
+  const forwarded = req.headers['x-forwarded-for'];
+  if (forwarded) {
+    const ipList = forwarded.split(',');
+    return ipList[ipList.length - 1];
+  }
+  return req.connection.remoteAddress;
+};
 
 const app = express();
 
 app.get('/', (req, res) => {
-    // Get the user's IP address from the request
-    const userIp = req.ip;
+    const userIp = getClientIp(req);
 
-    // Use the geoip.lookup function to get location information
-    const geo = geoip.lookup(userIp);
+  try {
+    const response = await axios.get(`https://ipinfo.io/${userIp}/json?token=19ddd5f80d1757`);
+    const { loc } = response.data;
 
-    // Check if the location information is available
-    
-    // Check if the location information is available
-    if (geo) {
-        console.log('User IP:', userIp);
-        console.log('User Location:', geo);
-        res.json(geo); // Send location information as JSON response
+    if (loc) {
+      const [latitude, longitude] = loc.split(',').map(parseFloat);
+      console.log('User IP:', userIp);
+      console.log('Latitude:', latitude);
+      console.log('Longitude:', longitude);
+      res.json({ latitude, longitude });
     } else {
-        console.log('Location not found for the given IP.');
-        res.status(404).json({ error: 'Location not found for the given IP' });
+      console.error('Location information not found.');
+      res.status(404).json({ error: 'Location not found for the given IP' });
     }
+  } catch (error) {
+    console.error('Error fetching location:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
